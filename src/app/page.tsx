f34633c62854c3391594e798c7664770096e5ee1
StarEdit <phuongtan9920@gmail.com>
2024-01-06 12:26:53 +0700

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useTheme } from "next-themes";
+
+const Home = () => {
+  const { systemTheme, theme, setTheme } = useTheme();
+  const currentTheme = theme === "system" ? systemTheme : theme;
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
+        <div></div>
+        <h3 className="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight">
+          Writes Upside-Down
+        </h3>
+        <p className="text-slate-500 dark:text-slate-400 mt-2 text-sm">
+          The Zero Gravity Pen can be used to write in any orientation,
+          including upside-down. It even works in outer space.
+        </p>
+      </div>
+      <div
+        onClick={() => (theme == "dark" ? setTheme("light") : setTheme("dark"))}
+      >
+        change
+      </div>
+    </main>
+  );
+};
+
+export default Home;
